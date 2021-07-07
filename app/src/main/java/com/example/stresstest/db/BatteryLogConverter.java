2b687d5a648f0fa6f9080dba4b19ffdacae674d1
ChapaevAnton <virsus2009@gmail.com>
2021-07-07 14:10:17 +0700

    add database
    *you need to add conversion methods lineValueSet

--- app/src/main/java/com/example/stresstest/db/BatteryLogConverter.java
+++ app/src/main/java/com/example/stresstest/db/BatteryLogConverter.java
@@ -8,14 +8,21 @@ import androidx.room.TypeConverter;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 public class BatteryLogConverter {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @TypeConverter
-    public long fromLocalDateTimeToLong(LocalDateTime localDateTime) {
-        ZonedDateTime zdt = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+    public long fromLocalDateTimeToLong(LocalDateTime dateTime) {
+        ZonedDateTime zdt = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
         return zdt.toInstant().toEpochMilli();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @TypeConverter
+    public LocalDateTime fromLongToLocalDateTime(long dateTime){
+        return new Date(dateTime).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
 }
